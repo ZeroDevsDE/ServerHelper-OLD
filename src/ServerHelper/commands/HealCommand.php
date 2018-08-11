ebf0491b2f6c98599d10a94f7a7032c2a792cb19
HonorGamerHD <37555011+HonorGamerHD@users.noreply.github.com>
2018-08-12 01:40:32 +0200

    Fixed formatting + Added Permission

--- src/ServerHelper/commands/HealCommand.php
+++ src/ServerHelper/commands/HealCommand.php
@@ -9,25 +9,34 @@
 
 namespace ServerHelper\commands;
 
-use ServerHelper\ServerHelper;
-use ServerHelper\CommandBase;
 use pocketmine\plugin\PluginCommand;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as SH;
+use pocketmine\Player;
+use ServerHelper\ServerHelper;
+use ServerHelper\CommandBase;
 
 class HealCommand extends CommandBase
 {
-   public $prefix = SH::GRAY . "» " . SH::AQUA . "S-H" . SH::GRAY . " » ";
-   
+	public $prefix = SH::GRAY . "» " . SH::AQUA . "S-H" . SH::GRAY . " » ";
+	
    public function __construct() 
    {
-      parent::__construct("heal", "heal command", "/heal", ["h"]);
+	parent::__construct("heal", "heal command", "/heal", ["h"]);
    }
 
    public function execute(CommandSender $sender, string $commandLabel, array $args)
    {
-   	$sender->setHealth(20);
-   	$sender->sendMessage($this->prefix . "You were healed!");
+   	if($sender instanceof Player){
+   		if($sender->hasPermission("serverhelper.commands.heal")){
+				$sender->setHealth($sender->getMaxHealth());
+				$sender->sendMessage($this->prefix . "You were healed!");
+			}else{
+				$sender->sendMessage($this->prefix . "You dont have the Permission to use this Command!");
+			}
+		}else{
+			$sender->sendMessage($this->prefix . "This Command is Only for Players!");
+		}
    }
 }

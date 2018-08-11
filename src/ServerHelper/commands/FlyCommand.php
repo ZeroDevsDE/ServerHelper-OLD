54a4063e6a9fb7356137cf3ade5af8b77d4cfeca
HonorGamerHD <37555011+HonorGamerHD@users.noreply.github.com>
2018-08-12 01:22:37 +0200

    Fixed formatting + Added Permission

--- src/ServerHelper/commands/FlyCommand.php
+++ src/ServerHelper/commands/FlyCommand.php
@@ -16,25 +16,27 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as SH;
 
-class FlyCommand extends CommandBase{
-	
+class FlyCommand extends CommandBase
+{
 	public $prefix = SH::GRAY . "» " . SH::AQUA . "S-H" . SH::GRAY . " » ";
 	
-   public function __construct() 
+	public function __construct() 
    {
-      parent::__construct("fly", "fly command", "/fly", ["fly"]);
+		parent::__construct("fly", "fly command", "/fly", ["fly"]);
    }
-   public function execute(CommandSender $sender, string $commandLabel, array $args){
-                if ($sender->hasPermission("serverhelper.commands.fly")) {
-                    if (!$sender->getAllowFlight()) {
-                        $sender->setAllowFlight(true);
-                        $sender->sendMessage($this->prefix . SH::GREEN . "You can now fly.");
-                    } else {
-                        if ($sender->getAllowFlight()) {
-                            $sender->setAllowFlight(false);
-                            $sender->sendMessage($this->prefix . SH::RED . "You can no longer fly.");
-                        }
-                    }
-                }
-        }
+	public function execute(CommandSender $sender, string $commandLabel, array $args){
+			if($sender->hasPermission("serverhelper.commands.fly")) {
+				if(!$sender->getAllowFlight()){
+					$sender->setAllowFlight(true);
+					$sender->sendMessage($this->prefix . SH::GREEN . "You can now fly.");
+				}else{
+				if($sender->getAllowFlight()){
+					$sender->setAllowFlight(false);
+					$sender->sendMessage($this->prefix . SH::RED . "You can no longer fly.");
+					}
+				}
+			}else{
+				$sender->sendMessage($this->prefix . "You dont have the Permission to use this Command!");
+		}
+	}
 }

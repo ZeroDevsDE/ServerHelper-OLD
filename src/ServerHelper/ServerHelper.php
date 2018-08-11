c5227ca6f0c2e0166ed5e70e1d800cc4f9b8a672
HonorGamerHD <37555011+HonorGamerHD@users.noreply.github.com>
2018-08-11 13:01:29 +0200

    added CommandLoader

--- src/ServerHelper/ServerHelper.php
+++ src/ServerHelper/ServerHelper.php
@@ -14,6 +14,7 @@ use pocketmine\plugin\PluginBase;
 class ServerHelper extends PluginBase{
 	public function onEnable(){
 		$this->Banner();
+		$this->CommandLoader();
 		$this->getLogger()->info("Server-Helper was activated!");
 	}
 	private function Banner(){
@@ -26,5 +27,8 @@ class ServerHelper extends PluginBase{
 		"╚╝───╚╝╚╝╚╝╚═══╝╚═╝╚═╝╚╝───╚═══╝╚╝╚═╝──╚╝──╚═══╝"
 		);
 		$this->getLogger()->info($banner);
+	}
+	private function CommandLoader(){
+		$this->getServer()->getCommandMap()->registerAll("ServerHelper", [new HealCommand()]);
 	}
 }

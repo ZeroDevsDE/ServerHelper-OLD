7bec956e2b259e3414c78632c983c67a1c7a8468
HonorGamerHD <37555011+HonorGamerHD@users.noreply.github.com>
2018-08-11 12:57:44 +0200

    added CommandBase.php

---
+++ src/ServerHelper/CommandBase.php
@@ -0,0 +1,29 @@
+<?php
+
+#╔═══╗╔═╗╔═╗╔═══╗╔═╗╔═╗╔═══╗╔═══╗╔═══╗╔════╗╔═══╗
+#║╔═╗║║║╚╝║║║╔══╝╚╗╚╝╔╝║╔═╗║║╔══╝║╔═╗║║╔╗╔╗║║╔═╗║
+#║╚═╝║║╔╗╔╗║║╚══╗─╚╗╔╝─║╚═╝║║╚══╗║╚═╝║╚╝║║╚╝║╚══╗
+#║╔══╝║║║║║║║╔══╝─╔╝╚╗─║╔══╝║╔══╝║╔╗╔╝──║║──╚══╗║
+#║║───║║║║║║║╚══╗╔╝╔╗╚╗║║───║╚══╗║║║╚╗──║║──║╚═╝║
+#╚╝───╚╝╚╝╚╝╚═══╝╚═╝╚═╝╚╝───╚═══╝╚╝╚═╝──╚╝──╚═══╝
+
+namespace ServerHelper;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use ServerHelper\ServerHelper;
+use ServerHelper\commands\HealCommand;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\plugin\Plugin;
+
+abstract class CommandBase extends Command implements PluginIdentifiableCommand {
+
+   public function __construct($name, $description = "", $usageMessage = null, $aliases = []){
+      parent::__construct($name, $description, $usageMessage, $aliases);
+   }
+   
+   public function getPlugin() : Plugin
+   {
+     return ServerHelper::getInstance();
+   }
+}

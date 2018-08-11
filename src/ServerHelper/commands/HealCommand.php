fd45fb1673ed0667b2d5dcdb3a8b9eb043541fe4
HonorGamerHD <37555011+HonorGamerHD@users.noreply.github.com>
2018-08-11 19:03:40 +0200

    added prefix

--- src/ServerHelper/commands/HealCommand.php
+++ src/ServerHelper/commands/HealCommand.php
@@ -14,9 +14,12 @@ use ServerHelper\CommandBase;
 use pocketmine\plugin\PluginCommand;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as SH;
 
 class HealCommand extends CommandBase
 {
+   public $prefix = SH::GRAY . "» " . SH::AQUA . "S-H" . SH::GRAY . " » ";
+   
    public function __construct() 
    {
       parent::__construct("heal", "heal command", "/heal", ["h"]);
@@ -25,6 +28,6 @@ class HealCommand extends CommandBase
    public function execute(CommandSender $sender, string $commandLabel, array $args)
    {
    	$sender->setHealth(20);
-   	$sender->sendMessage("You were healed!");
+   	$sender->sendMessage($this->prefix . "You were healed!");
    }
 }

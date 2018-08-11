1b7d28f18cb55e1a8bf36b29051b2aefcb8cefb1
HonorGamerHD <37555011+HonorGamerHD@users.noreply.github.com>
2018-08-12 00:32:40 +0200

    Fixed formatting + Added Permission

--- src/ServerHelper/commands/FeedCommand.php
+++ src/ServerHelper/commands/FeedCommand.php
@@ -15,17 +15,28 @@ use pocketmine\plugin\PluginCommand;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as SH;
+use pocketmine\Server;
+use pocketmine\Player;
 
-class FeedCommand extends CommandBase{ 
-   
-   public $prefix = SH::GRAY . "» " . SH::AQUA . "S-H" . SH::GRAY . " » ";
-   
-   public function __construct(){
-      parent::__construct("feed", "feed command", "/feed", ["f"]);
-   }
-   public function execute(CommandSender $sender, string $commandLabel, array $args)
+class FeedCommand extends CommandBase
+{
+	public $prefix = SH::GRAY . "» " . SH::AQUA . "S-H" . SH::GRAY . " » ";
+	
+	public function __construct() 
    {
-   	$sender->setFood(20);
-   	$sender->sendMessage($this->prefix . "You were fed!");
+		parent::__construct("feed", "feed command", "/feed", ["f"]);
    }
+	public function execute(CommandSender $sender, string $commandLabel, array $args)
+   {
+		if($sender instanceof Player){
+			if($sender->hasPermission("serverhelper.command.feed")){
+				$sender->setFood(20);
+				$sender->sendMessage($this->prefix . "You were fed!");
+			}else{
+				$sender->sendMessage($this->prefix . "You dont have the Permission to use this Command!");
+			}
+		}else{
+			$sender->sendMessage($this->prefix . "This Command is Only for Players!");
+		}
+	}
 }

fa997660a9594cf7b454cdbb21ebb22b7d80e89b
HonorGamerHD <37555011+HonorGamerHD@users.noreply.github.com>
2018-08-12 00:20:54 +0200

    Added Permission + ConsoleBlock

--- src/ServerHelper/commands/BroadcastCommand.php
+++ src/ServerHelper/commands/BroadcastCommand.php
@@ -18,7 +18,8 @@ use pocketmine\utils\TextFormat as SH;
 use pocketmine\Server;
 use pocketmine\Player;
 
-class BroadcastCommand extends CommandBase{
+class BroadcastCommand extends CommandBase
+{
 	
 	public $bcprefix = SH::GRAY . "» " . SH::AQUA . SH::BOLD . "Broadcast". SH::RESET . SH::GRAY . " » ";
 	public $prefix = SH::GRAY . "» " . SH::AQUA . "S-H" . SH::GRAY . " » ";
@@ -29,10 +30,18 @@ class BroadcastCommand extends CommandBase{
    }
    public function execute(CommandSender $sender, string $commandLabel, array $args)
    {
-   	if(!empty($args[0])){
-   		$sender->getServer()->broadcastMessage($this->bcprefix . $args[0]);
-   	}else{
-			$sender->sendMessage($this->prefix . "Usage: /broadcast <message>");
+   	if($sender instanceof Player){
+			if($sender->hasPermission("serverhelper.command.broadcast")){
+				if(!empty($args[0])){
+   				$sender->getServer()->broadcastMessage($this->bcprefix . $args[0]);
+   			}else{
+					$sender->sendMessage($this->prefix . "Usage: /broadcast <message>");
+				}
+			}else{
+				$sender->sendMessage($this->prefix . "You dont have the Permission to use this Command!");
+			}
+	  	}else{
+			$sender->sendMessage($this->prefix . "This Command is Only for Players!");
 		}
-   }
+	}
 }

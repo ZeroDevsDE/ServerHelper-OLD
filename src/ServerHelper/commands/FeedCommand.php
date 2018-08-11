01ea07f9e966be330f470315cd7a391a7f77fd4f
HonorGamerHD <37555011+HonorGamerHD@users.noreply.github.com>
2018-08-11 19:00:54 +0200

    added prefix

--- src/ServerHelper/commands/FeedCommand.php
+++ src/ServerHelper/commands/FeedCommand.php
@@ -15,15 +15,16 @@ use pocketmine\plugin\PluginCommand;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
-class FeedCommand extends CommandBase
-{
-   public function __construct() 
-   {
+class FeedCommand extends CommandBase{ 
+   
+   public $prefix = SH::GRAY . "» " . SH::AQUA . "S-H" . SH::GRAY . " » ";
+   
+   public function __construct(){
       parent::__construct("feed", "feed command", "/feed", ["f"]);
    }
    public function execute(CommandSender $sender, string $commandLabel, array $args)
    {
    	$sender->setFood(20);
-   	$sender->sendMessage("You were fed!");
+   	$sender->sendMessage($this->prefix . "You were fed!");
    }
 }

5b3e6508ee2a136e42005f3b9b0d116a223dabec
HonorGamerHD <37555011+HonorGamerHD@users.noreply.github.com>
2018-08-12 19:19:49 +0200

    Added NickOffCommand.php

---
+++ src/ServerHelper/commands/NickOffCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+#╔═══╗╔═╗╔═╗╔═══╗╔═╗╔═╗╔═══╗╔═══╗╔═══╗╔════╗╔═══╗
+#║╔═╗║║║╚╝║║║╔══╝╚╗╚╝╔╝║╔═╗║║╔══╝║╔═╗║║╔╗╔╗║║╔═╗║
+#║╚═╝║║╔╗╔╗║║╚══╗─╚╗╔╝─║╚═╝║║╚══╗║╚═╝║╚╝║║╚╝║╚══╗
+#║╔══╝║║║║║║║╔══╝─╔╝╚╗─║╔══╝║╔══╝║╔╗╔╝──║║──╚══╗║
+#║║───║║║║║║║╚══╗╔╝╔╗╚╗║║───║╚══╗║║║╚╗──║║──║╚═╝║
+#╚╝───╚╝╚╝╚╝╚═══╝╚═╝╚═╝╚╝───╚═══╝╚╝╚═╝──╚╝──╚═══╝
+
+namespace ServerHelper\commands;
+
+use ServerHelper\CommandBase;;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as SH;
+use pocketmine\Player;
+
+class NickOffCommand extends CommandBase
+{
+    public $prefix = SH::GRAY . "» " . SH::AQUA . "S-H" . SH::GRAY . " » ";
+
+    public function __construct()
+    {
+        parent::__construct("nicknameoff", "nicknameoff command", "/nicknameoff", ["nickoff"]);
+    }
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if($sender instanceof Player){
+            if($sender->hasPermission("serverhelper.command.nick")){
+                $sender->setDisplayName($sender->getName());
+                $sender->setNameTag($sender->getName());
+                $sender->sendMessage($this->prefix . "Your Nickname was reset to " . SH::GREEN . $sender->getName() . SH::GRAY . "!");
+            }else{
+                $sender->sendMessage($this->prefix . "You dont have the Permission to use this Command!");
+            }
+        }else{
+            $sender->sendMessage($this->prefix . "This Command is Only for Players!");
+        }
+    }
+}

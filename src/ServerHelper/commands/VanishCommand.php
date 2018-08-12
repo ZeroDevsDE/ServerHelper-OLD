ff3d574d8fc5bf2f6a7a7408c6d24a027bb6a97c
HonorGamerHD <37555011+HonorGamerHD@users.noreply.github.com>
2018-08-12 20:17:04 +0200

    Added VanishCommand.php

---
+++ src/ServerHelper/commands/VanishCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+#╔═══╗╔═╗╔═╗╔═══╗╔═╗╔═╗╔═══╗╔═══╗╔═══╗╔════╗╔═══╗
+#║╔═╗║║║╚╝║║║╔══╝╚╗╚╝╔╝║╔═╗║║╔══╝║╔═╗║║╔╗╔╗║║╔═╗║
+#║╚═╝║║╔╗╔╗║║╚══╗─╚╗╔╝─║╚═╝║║╚══╗║╚═╝║╚╝║║╚╝║╚══╗
+#║╔══╝║║║║║║║╔══╝─╔╝╚╗─║╔══╝║╔══╝║╔╗╔╝──║║──╚══╗║
+#║║───║║║║║║║╚══╗╔╝╔╗╚╗║║───║╚══╗║║║╚╗──║║──║╚═╝║
+#╚╝───╚╝╚╝╚╝╚═══╝╚═╝╚═╝╚╝───╚═══╝╚╝╚═╝──╚╝──╚═══╝
+
+namespace ServerHelper\commands;
+
+use ServerHelper\CommandBase;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as SH;
+use pocketmine\Player;
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
+
+class VanishCommand extends CommandBase
+{
+    public $prefix = SH::GRAY . "» " . SH::AQUA . "S-H" . SH::GRAY . " » ";
+
+    public function __construct()
+    {
+        parent::__construct("vanish", "vanish command", "/vanish", ["v"]);
+    }
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if($sender instanceof Player){
+            if($sender->hasPermission("serverhelper.command.vanish")){
+                $sender->setDisplayName("");
+                $sender->addEffect(new EffectInstance(Effect::getEffect(Effect::NIGHT_VISION), (99999999*20), (1), (false)));
+                $sender->addEffect(new EffectInstance(Effect::getEffect(Effect::INVISIBILITY), (99999999*20), (1), (false)));
+                $sender->sendMessage($this->prefix . "You are now Vanished!");
+            }else{
+                $sender->sendMessage($this->prefix . "You dont have the Permission to use this Command!");
+            }
+        }else{
+            $sender->sendMessage($this->prefix . "This Command is Only for Players!");
+        }
+    }
+}

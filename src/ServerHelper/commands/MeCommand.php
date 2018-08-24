5dacaf96081a4d12a6d210738afb42ead89adfd0
HonorGamerHD <niebuhrsteve@gmail.com>
2018-08-24 10:27:09 +0200

    Fixed MeCommand

--- src/ServerHelper/commands/MeCommand.php
+++ src/ServerHelper/commands/MeCommand.php
@@ -12,8 +12,8 @@ namespace ServerHelper\commands;
 use ServerHelper\ServerHelper;
 use ServerHelper\CommandBase;
 use pocketmine\plugin\PluginCommand;
-use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\utils\TextFormat as SH;
 use pocketmine\Server;
 use pocketmine\Player;
@@ -28,18 +28,14 @@ class MeCommand extends CommandBase
    }
 	public function execute(CommandSender $sender, string $commandLabel, array $args)
    {
-		if($sender instanceof Player){
-			if($sender->hasPermission("serverhelper.command.me")){
-				if(!empty($args[0])){
-					$sender->getServer()->broadcastMessage(" * " . $sender->getName() . " " . $args[0]);
-				}else{
-					$sender->sendMessage($this->prefix . "Usage: /me <message>");
-				}
+		if($sender->hasPermission("serverhelper.command.me")){
+			if(!empty($args[0])){
+				$sender->getServer()->broadcastMessage("* " . $sender->getName() . " " . implode(" ", $args));
 			}else{
-				$sender->sendMessage($this->prefix . "You dont have the Permission to use this Command!");
+				$sender->sendMessage($this->prefix . "Usage: /me <message>");
 			}
 		}else{
-			$sender->sendMessage($this->prefix . "This Command is Only for Players!");
+			$sender->sendMessage($this->prefix . "You dont have the Permission to use this Command!");
 		}
 	}
 }
